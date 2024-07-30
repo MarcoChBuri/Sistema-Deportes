bd79f2e11603b1a3a3772e1a9edbdddf04034318
Omar <marco.chiliguano@unl.edu.ec>
2024-07-30 10:37:48 -0500

    equipos

--- Deportes/tablaPosisiones/models.py
+++ Deportes/tablaPosisiones/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from campeonato.models import Equipo
 
 # Create your models here.
+class TablaPosisiones(models.Model):
+    equipo = models.CharField(max_length=100)
+    puntos = models.IntegerField()
+    partidos_jugados = models.IntegerField()
+    partidos_ganados = models.IntegerField()
+    partidos_empatados = models.IntegerField()
+    partidos_perdidos = models.IntegerField()
+    goles_favor = models.IntegerField()
+    goles_contra = models.IntegerField()
+    diferencia_goles = models.IntegerField()
+
+    def __str__(self):
+        return self.equipo
+class Encuentro(models.Model):
+    equipo_local = models.ManyToManyField(Equipo, related_name='encuentros_locales')
+    equipo_visitante = models.ManyToManyField(Equipo, related_name='encuentros_visitantes')
+    goles_local = models.IntegerField()
+    goles_visitante = models.IntegerField()
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.equipo_local + " vs " + self.equipo_visitante
